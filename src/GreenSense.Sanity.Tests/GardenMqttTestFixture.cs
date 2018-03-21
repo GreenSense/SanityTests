1b7c6b84118cca65fa2c93734546c0eac0d64b70
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-21 00:22:10 +0000

    Added mqtt test fixture

---
+++ src/GreenSense.Sanity.Tests/GardenMqttTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace GreenSense.Sanity.Tests
+{
+	[TestFixture]
+	public class GardenMqttTestFixture : BaseTestFixture
+	{
+		[Test]
+		public void Test_PingGarden()
+		{
+			var token = Environment.GetEnvironmentVariable ("TESTTOKEN");
+
+			Console.WriteLine (token);
+		}
+
+	}
+}
+
