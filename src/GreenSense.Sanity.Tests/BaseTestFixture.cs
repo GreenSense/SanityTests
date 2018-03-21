dd8ce7368277872b636791b9ad18399ff3b84789
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-21 00:18:48 +0000

    Initial commit

---
+++ src/GreenSense.Sanity.Tests/BaseTestFixture.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+
+namespace GreenSense.Sanity.Tests
+{
+    public abstract class BaseTestFixture
+    {
+        public string OriginalDirectory;
+        public string TemporaryDirectory;
+
+        public bool DeleteTemporaryDirectory = true;
+
+        public BaseTestFixture ()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            OriginalDirectory = Environment.CurrentDirectory;
+
+            TemporaryDirectory = new TemporaryDirectoryCreator ().Create ();
+
+            Directory.SetCurrentDirectory (TemporaryDirectory);
+
+            Console.WriteLine ("Current directory:");
+            Console.WriteLine (TemporaryDirectory);
+            Console.WriteLine ();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine ("Cleaning up...");
+
+            Directory.SetCurrentDirectory (OriginalDirectory);
+
+            if (DeleteTemporaryDirectory && TemporaryDirectory.ToLower().Contains("-tmp")) {
+                Directory.Delete (TemporaryDirectory, true);
+            }
+        }
+    }
+}
+
---
+++ src/GreenSense.Sanity.Tests/PingGardenTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace GreenSense.Sanity.Tests
+{
+    [TestFixture]
+    public class PingGardenTestFixture : BaseTestFixture
+    {
+        [Test]
+        public void Test_PingGarden()
+        {
+			PingHost ("greensense.hopto.org");
+        }
+
+		public static bool PingHost(string nameOrAddress)
+		{
+			bool pingable = false;
+			Ping pinger = new Ping();
+			try
+			{
+				PingReply reply = pinger.Send(nameOrAddress);
+				pingable = reply.Status == IPStatus.Success;
+			}
+			catch (PingException ex)
+			{
+				Assert.Fail (ex.ToString ());
+			}
+			return pingable;
+		}
+    }
+}
+
