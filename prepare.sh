351e0e0466378e1ca96e1f6fd3591b08cbd02eea
CompulsiveCoder <compulsivecoder@gmail.com>
2018-04-06 23:46:10 +0000

    Updated prepare script

--- prepare.sh
+++ prepare.sh
@@ -4,10 +4,7 @@ echo "Dir: $PWD"
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/ubuntu stable-xenial main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
 
-# Create the source list file then copy (directly creating causes permission denied errors on c9.io)
-sudo echo "deb http://download.mono-project.com/repo/debian wheezy main" > mono-xamarin.list
-sudo cp mono-xamarin.list /etc/apt/sources.list.d/mono-xamarin.list
-
-sudo apt-get update -qq
+sudo apt-get update -qq && \
 sudo apt-get install -y --no-install-recommends git wget mono-complete msbuild ca-certificates-mono
